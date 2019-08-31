960d8f867775b46d1af79c2475305fd22a7e1a9b
primys <a2uso@tuta.io>
2019-08-30 21:22:14 -0700

    Add interface for Purchases contract

---
+++ src/main/kotlin/io/usoamic/usoamickotlinjs/core/Purchases.kt
@@ -0,0 +1,19 @@
+package io.usoamic.usoamickotlinjs.core
+
+import io.usoamic.usoamickotlinjs.model.Purchase
+import io.usoamic.web3kt.core.contract.response.CallResponse
+import io.usoamic.web3kt.core.contract.response.ExecuteResponse
+
+interface Purchases {
+    @JsName("makePurchase")
+    fun makePurchase(appId: String, purchaseId: String, cost: String): ExecuteResponse
+
+    @JsName("getPurchaseByAddress")
+    fun getPurchaseByAddress(address: String, id: String): CallResponse<Purchase>
+
+    @JsName("getLastPurchaseId")
+    fun getLastPurchaseId(address: String): String
+
+    @JsName("getNumberOfPurchasesByAddress")
+    fun getNumberOfPurchasesByAddress(address: String): String
+}
