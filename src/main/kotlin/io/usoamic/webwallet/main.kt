41f39850214003b05ed4a555362e5672ee6b32ea
primys <a2uso@tuta.io>
2019-08-31 09:47:42 -0700

    Add listeners for buttons and jQuery objects

--- src/main/kotlin/io/usoamic/webwallet/main.kt
+++ src/main/kotlin/io/usoamic/webwallet/main.kt
@@ -1,11 +1,56 @@
 package io.usoamic.webwallet
 
+import js.externals.jquery.JQuery
 import js.externals.jquery.jQuery
+import org.w3c.dom.HTMLElement
 
-fun setResult(str: String) {
-    jQuery("#result").text(str)
+val loader = jQuery(".loader")
+val firstView = jQuery("#first_view")
+val addWalletView = jQuery("#add_wallet_view")
+val createWalletView = jQuery("#create_wallet_view")
+val dashboardView = jQuery("#dashboard_view")
+
+/*
+ * Common
+ */
+var currentView = firstView
+
+/*
+ * FirstPage
+ */
+val addButton = jQuery("#add_button")
+val createButton = jQuery("#create_button")
+
+/*
+ * AddWalletPage
+ */
+
+
+/*
+ * CreateWalletPage
+ */
+
+/*
+ * DashboardPage
+ */
+fun main() {
+    loader.delay(1500).fadeOut()
+    setListeners()
 }
 
-fun main() {
-    setResult("ALFA")
+private fun setListeners() {
+    addButton.on("click") {
+        println("onClick: addButton")
+        addWalletView.showIt()
+    }
+
+    createButton.on("click") {
+        createWalletView.showIt()
+    }
+}
+
+fun JQuery<HTMLElement>.showIt() {
+    currentView.hide()
+    this.show()
+    currentView = this
 }
