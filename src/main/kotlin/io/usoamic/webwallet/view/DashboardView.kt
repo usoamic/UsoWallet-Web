ab55b29c517f2ce148e2f30e1cddf343e479ce8c
primys <a2uso@tuta.io>
2019-09-01 12:23:31 -0700

    Add DashboardView

---
+++ src/main/kotlin/io/usoamic/webwallet/view/DashboardView.kt
@@ -0,0 +1,26 @@
+package io.usoamic.webwallet.view
+
+import io.usoamic.usoamickotlinjs.other.Config
+import io.usoamic.webwallet.base.Application
+import io.usoamic.webwallet.base.View
+import io.usoamic.webwallet.exception.WalletNotFoundException
+import io.usoamic.webwallet.model.Account
+import js.externals.jquery.jQuery
+import kotlin.browser.localStorage
+
+class DashboardView(application: Application) : View(application) {
+    override val view = jQuery("#dashboard_view")
+    val account get() =  localStorage.getItem(Config.ACCOUNT_FILENAME)?.let {
+            JSON.parse<Account>(it)
+    } ?: throw WalletNotFoundException()
+
+    init {
+        println("address: ${account.address}")
+    }
+
+    companion object {
+        fun newInstance(application: Application) {
+            return application.open(DashboardView(application))
+        }
+    }
+}
