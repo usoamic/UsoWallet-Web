9294757cf9209ae70b3b47d9f47f44818f036872
primys <a2uso@tuta.io>
2019-09-01 12:37:54 -0700

    Implement CreateWalletView

--- src/main/kotlin/io/usoamic/webwallet/view/CreateWalletView.kt
+++ src/main/kotlin/io/usoamic/webwallet/view/CreateWalletView.kt
@@ -1,13 +1,45 @@
 package io.usoamic.webwallet.view
 
-import io.usoamic.webwallet.App
 import io.usoamic.webwallet.base.Application
 import io.usoamic.webwallet.base.View
+import js.externals.jquery.extension.onClick
 import js.externals.jquery.jQuery
+import io.usoamic.web3kt.wallet.Wallet
+import io.usoamic.webwallet.util.Async
+import js.externals.jquery.extension.removeHidden
+import js.externals.jquery.extension.showLoading
 
 class CreateWalletView(application: Application) : View(application) {
     override val view = jQuery("#create_wallet_view")
-    private val newPrivateKeyElement = jQuery("#new_pk_element")
+    private val nextBtn = jQuery("#next_button")
+    private val privateKeyElement = jQuery("#new_pk_element")
+    private val addressElement = jQuery("#new_address_element")
+
+    init {
+        preparePrivateKey()
+        setListeners()
+    }
+
+    override fun startLoading() {
+        privateKeyElement.showLoading()
+        addressElement.showLoading()
+    }
+
+    private fun preparePrivateKey() {
+        startLoading()
+        Async.launch {
+            val wallet = Wallet.generate()
+            privateKeyElement.text(wallet.getPrivateKeyAsString())
+            addressElement.text(wallet.getAddressAsString())
+            nextBtn.removeHidden()
+        }
+    }
+
+    private fun setListeners() {
+        nextBtn.onClick {
+            AddWalletView.newInstance(application)
+        }
+    }
 
     companion object {
         fun newInstance(application: Application) {
