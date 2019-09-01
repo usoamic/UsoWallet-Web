59f27b7e8c04548d7e3a2546ef92e5f17ded26a3
primys <a2uso@tuta.io>
2019-09-01 12:24:10 -0700

    Implement AddWalletView

--- src/main/kotlin/io/usoamic/webwallet/view/AddWalletView.kt
+++ src/main/kotlin/io/usoamic/webwallet/view/AddWalletView.kt
@@ -1,26 +1,66 @@
 package io.usoamic.webwallet.view
 
+import io.usoamic.web3kt.kt2js.require
+import io.usoamic.web3kt.util.extension.removeHexPrefixIfExist
 import io.usoamic.webwallet.base.Application
 import io.usoamic.webwallet.base.View
+import io.usoamic.webwallet.util.ValidateUtil
 import js.externals.jquery.extension.onClick
 import js.externals.jquery.jQuery
+import io.usoamic.web3kt.wallet.extension.fromPrivateKey
+import io.usoamic.web3kt.wallet.Wallet
+import io.usoamic.webwallet.util.Async
+import js.externals.jquery.extension.startLoading
+import js.externals.jquery.extension.stopLoading
+import js.externals.toastr.extensions.error
+import js.externals.toastr.toastr
+import io.usoamic.web3kt.wallet.extension.toJsonString
+import kotlin.browser.localStorage
+import io.usoamic.usoamickotlinjs.other.Config
 
 class AddWalletView(application: Application) : View(application) {
     override val view = jQuery("#add_wallet_view")
-    val privateKeyInput = jQuery("#add_pk_input")
-    val passwordInput = jQuery("#add_password_input")
-    val confirmInput = jQuery("#add_confirm_password_input")
-    val saveBtn = jQuery("#save_button")
+    private val privateKeyInput = jQuery("#add_pk_input")
+    private val passwordInput = jQuery("#add_password_input")
+    private val confirmPasswordInput = jQuery("#add_confirm_password_input")
+    private val saveBtn = jQuery("#save_button")
 
     init {
-
+        setListeners()
     }
 
-    fun setListeners() {
+    override fun startLoading() {
+        saveBtn.startLoading()
+    }
+
+    override fun stopLoading() {
+        saveBtn.stopLoading(true)
+    }
+
+    private fun setListeners() {
         saveBtn.onClick {
+            startLoading()
+            try {
+                val privateKey = privateKeyInput.content().removeHexPrefixIfExist()
+                val password = passwordInput.content()
+                val confirmPassword = confirmPasswordInput.content()
 
+                ValidateUtil.validatePrivateKey(privateKey)
+                    .validatePassword(password)
+                    .validateConfirmPassword(confirmPassword)
+
+                Async.launch {
+                    val json = Wallet.fromPrivateKey(privateKey).toV3(password).toJsonString()
+                    localStorage.setItem(Config.ACCOUNT_FILENAME, json)
+                    stopLoading()
+                    DashboardView.newInstance(application)
+                }
+
+            } catch (e: Throwable) {
+                saveBtn.stopLoading(false)
+                toastr.error(e.message)
+            }
         }
-
     }
 
     companion object {
