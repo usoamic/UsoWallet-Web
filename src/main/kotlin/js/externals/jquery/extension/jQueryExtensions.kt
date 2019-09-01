13fee0ea5a72ba63ecfc393994a05bfc6c224635
primys <a2uso@tuta.io>
2019-09-01 12:22:18 -0700

    Add startLoading() and stopLoading()

--- src/main/kotlin/js/externals/jquery/extension/jQueryExtensions.kt
+++ src/main/kotlin/js/externals/jquery/extension/jQueryExtensions.kt
@@ -6,3 +6,17 @@ import org.w3c.dom.HTMLElement
 fun JQuery<HTMLElement>.onClick(callback: () -> Unit) {
     this.on("click", callback)
 }
+
+fun JQuery<HTMLElement>.startLoading() {
+    this.data("text", this.html())
+    this.html("<i class=\"fa fa-spinner fa-spin\"></i> Please wait...")
+    this.prop("disabled", true)
+}
+
+fun JQuery<HTMLElement>.stopLoading(disable: Boolean = false) {
+    val text = this.data("text") as String
+    if(text.isNotEmpty()) {
+        this.html(text)
+        this.prop("disabled", disable)
+    }
+}
