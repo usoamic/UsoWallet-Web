fb147ddb1bcd565bff0e3f910c83d3e5978c609b
primys <a2uso@tuta.io>
2019-09-07 14:10:52 -0700

    Fix some issues

--- src/main/kotlin/io/usoamic/webwallet/view/WithdrawView.kt
+++ src/main/kotlin/io/usoamic/webwallet/view/WithdrawView.kt
@@ -4,6 +4,7 @@ import io.usoamic.usoamickotlinjs.other.Config
 import io.usoamic.web3kt.bignumber.BigNumber
 import io.usoamic.web3kt.bignumber.BigNumberValue
 import io.usoamic.web3kt.buffer.Buffer
+import io.usoamic.web3kt.buffer.extension.fromHex
 import io.usoamic.web3kt.buffer.extension.toHex
 import io.usoamic.web3kt.core.contract.model.EstimateGasOption
 import io.usoamic.web3kt.core.contract.util.Coin
@@ -11,14 +12,19 @@ import io.usoamic.web3kt.tx.Transaction
 import io.usoamic.web3kt.tx.block.DefaultBlockParameterName
 import io.usoamic.web3kt.tx.model.RawTransaction
 import io.usoamic.web3kt.util.extension.addHexPrefix
+import io.usoamic.web3kt.util.extension.removeHexPrefixIfExist
 import io.usoamic.web3kt.wallet.Wallet
 import io.usoamic.webwallet.base.Application
 import io.usoamic.webwallet.base.WalletView
 import io.usoamic.webwallet.enums.Asset
+import io.usoamic.webwallet.exception.ValidateUtilException
 import io.usoamic.webwallet.util.Alert
 import io.usoamic.webwallet.util.ValidateUtil
 import js.externals.jquery.JQuery
+import js.externals.jquery.extension.clearVal
 import js.externals.jquery.extension.onClick
+import js.externals.jquery.extension.startLoading
+import js.externals.jquery.extension.stopLoading
 import js.externals.jquery.jQuery
 import org.w3c.dom.HTMLElement
 
@@ -46,82 +52,103 @@ class WithdrawView(application: Application) : WalletView(application) {
         }
     }
 
+    override fun startLoading() {
+        withdrawUsoBtn.startLoading()
+        withdrawEthBtn.startLoading()
+    }
+
+    override fun stopLoading() {
+        withdrawUsoBtn.stopLoading()
+        withdrawEthBtn.stopLoading()
+    }
+
+    override fun onException(t: Throwable) {
+        super.onException(t)
+        passwordInput.clearVal()
+        if (t !is ValidateUtilException) {
+            stopLoading()
+        }
+    }
+
     private fun withdraw(asset: Asset) {
-        val sAddress = addressInput.text()
-        val sAmount = amountInput.text()
-        val sPassword = passwordInput.text()
+        val sAddress = addressInput.content()
+        val sAmount = amountInput.content()
+        val sPassword = passwordInput.content()
 
         try {
             ValidateUtil.validateAddress(sAddress)
                 .validateTransferValue(sAmount)
                 .validatePassword(sPassword)
 
+            startLoading()
+
             when (asset) {
                 Asset.COIN -> sendEth(sAddress, sAmount, sPassword)
                 Asset.TOKEN -> sendUso(sAddress, sAmount, sPassword)
             }
+        } catch (t: Throwable) {
+            onException(t)
         }
-        catch (e: Throwable) {
-            onException(e)
-        }
-
     }
 
     private fun sendEth(sAddress: String, sAmount: String, sPassword: String) {
-                web3.eth.getTransactionCount(address, DefaultBlockParameterName.LATEST)
-                    .then { nonce ->
-                        web3.eth.estimateGas(EstimateGasOption(
-                            address,
-                            sAddress,
-                            BigNumber(sAmount)
-                        )).then { gasLimit ->
-                            val transaction = RawTransaction.createEtherTransaction(
-                                address,
-                                nonce,
-                                BigNumber(gasLimit),
-                                sAddress,
-                                Coin.fromCoin(sAmount).toBigNumber()
-                            )
-                            sendTransaction(transaction, sPassword)
-                        }
-                    }
+        web3.eth.getTransactionCount(address, DefaultBlockParameterName.LATEST)
+            .then { nonce ->
+                web3.eth.estimateGas(
+                    EstimateGasOption(
+                        address,
+                        sAddress,
+                        BigNumber(sAmount)
+                    )
+                ).then { gasLimit ->
+                    val transaction = RawTransaction.createEtherTransaction(
+                        address,
+                        nonce,
+                        BigNumber(gasLimit),
+                        sAddress,
+                        Coin.fromCoin(sAmount).toBigNumber()
+                    )
+                    sendTransaction(transaction, sPassword)
+                }
+            }
     }
 
     private fun sendUso(sAddress: String, sAmount: String, sPassword: String) {
-                web3.eth.getTransactionCount(address, DefaultBlockParameterName.LATEST)
-                    .then { nonce ->
-                        val response = methods.transfer(sAddress, Coin.fromCoin(sAmount).toStringSat())
-                        response.estimateGas(
-                            EstimateGasOption(
-                                address,
-                                sAddress,
-                                BigNumberValue.ZERO,
-                                response.encodeABI()
-                            )
+        web3.eth.getTransactionCount(address, DefaultBlockParameterName.LATEST)
+            .then { nonce ->
+                val response = methods.transfer(sAddress, Coin.fromCoin(sAmount).toStringSat())
+                response.estimateGas(
+                    EstimateGasOption(
+                        address,
+                        sAddress,
+                        BigNumberValue.ZERO,
+                        response.encodeABI()
+                    )
+                )
+                    .then { gasLimit ->
+                        val transaction = RawTransaction.createContractTransaction(
+                            address,
+                            nonce,
+                            BigNumber(gasLimit),
+                            Config.CONTRACT_ADDRESS,
+                            response.encodeABI()
                         )
-                            .then { gasLimit ->
-                                val transaction = RawTransaction.createContractTransaction(
-                                    address,
-                                    nonce,
-                                    BigNumber(gasLimit),
-                                    Config.CONTRACT_ADDRESS,
-                                    response.encodeABI()
-                                )
-                                sendTransaction(transaction, sPassword)
-                            }
+                        sendTransaction(transaction, sPassword)
                     }
+            }
 
     }
 
     private fun sendTransaction(transaction: RawTransaction, password: String) {
-        val privateKey = Wallet.fromV3(account, password).getPrivateKeyAsString()
+        val privateKey = Wallet.fromV3(account, password).getPrivateKeyAsString().removeHexPrefixIfExist()
 
         val tx = Transaction(transaction)
-        tx.sign(Buffer.from(privateKey, "hex"))
+        tx.sign(Buffer.fromHex(privateKey))
 
         val signedTransaction = tx.serialize()
         web3.eth.sendSignedTransaction(signedTransaction.toHex().addHexPrefix()).then {
             Alert.show(it.transactionHash)
+            stopLoading()
         }
     }
 
