60ac4bb0a9b97fb48f3647fcfa1dba49ff593077
primys <a2uso@tuta.io>
2019-09-07 08:11:08 -0700

    Move getting transaction to WalletView

--- src/main/kotlin/io/usoamic/webwallet/base/WalletView.kt
+++ src/main/kotlin/io/usoamic/webwallet/base/WalletView.kt
@@ -1,25 +1,26 @@
 package io.usoamic.webwallet.base
 
-import io.usoamic.usoamickotlinjs.core.TransactionExplorer
 import io.usoamic.usoamickotlinjs.core.Usoamic
-import io.usoamic.usoamickotlinjs.model.Transaction
+import io.usoamic.usoamickotlinjs.core.extension.getTransactionsByAddress
 import io.usoamic.usoamickotlinjs.other.Config
 import io.usoamic.usoamickotlinjs.other.Config.Companion.CONTRACT_ABI
 import io.usoamic.usoamickotlinjs.other.Config.Companion.NODE
 import io.usoamic.web3kt.core.Web3
-import io.usoamic.web3kt.core.contract.model.CallOption
+import io.usoamic.web3kt.core.contract.util.Coin
 import io.usoamic.web3kt.core.extension.newContract
 import io.usoamic.webwallet.enums.Page
+import io.usoamic.webwallet.enums.Transfer
 import io.usoamic.webwallet.exception.WalletNotFoundException
-import io.usoamic.webwallet.legacy.CONTRACT
 import io.usoamic.webwallet.model.Account
+import io.usoamic.webwallet.other.Timestamp
+import io.usoamic.webwallet.util.TxUtils
 import js.externals.jquery.extension.onClick
 import js.externals.jquery.jQuery
 import kotlin.browser.localStorage
 
 abstract class WalletView(application: Application) : View(application) {
     protected val web3 = Web3(NODE)
-    protected val contract = web3.newContract<Usoamic>(CONTRACT_ABI, CONTRACT)
+    private val contract = web3.newContract<Usoamic>(CONTRACT_ABI, "0x42210806DCA8E0C7A5Cff83192852eB0db4ce764")
     protected val methods = contract.methods
 
     private val logoutBtn = jQuery("#logout")
@@ -40,6 +41,31 @@ abstract class WalletView(application: Application) : View(application) {
         logoutBtn.onClick {
             localStorage.removeItem(Config.ACCOUNT_FILENAME)
             application.openPage(Page.FIRST)
+        }
+    }
+
+    protected fun getTransactions(callback: (List<List<Any>>) -> Unit) {
+        getTransactions(null, callback)
+    }
+
+    protected fun getTransactions(lastId: Long?, callback: (List<List<Any>>) -> Unit) {
+        methods.getTransactionsByAddress(address, lastId) {
+            val list = mutableListOf<List<Any>>()
+            it.forEach { tx ->
+                val txType = TxUtils.getTxType(address, tx.from)
+                list.add(
+                    listOf(
+                        txType.toPlainString(),
+                        when (txType) {
+                            Transfer.DEPOSIT -> tx.from
+                            Transfer.WITHDRAWAL -> tx.to
+                        },
+                        Coin.fromSat(tx.value).toPlainString(),
+                        Timestamp.fromBigNumber(tx.timestamp).toLocaleString()
+                    )
+                )
+            }
+            callback(list)
         }
     }
 }
--- src/main/kotlin/io/usoamic/webwallet/view/DashboardView.kt
+++ src/main/kotlin/io/usoamic/webwallet/view/DashboardView.kt
@@ -1,12 +1,7 @@
 package io.usoamic.webwallet.view
 
-import io.usoamic.usoamickotlinjs.core.extension.getTransactionsByAddress
-import io.usoamic.web3kt.core.contract.util.Coin
 import io.usoamic.webwallet.base.Application
 import io.usoamic.webwallet.base.WalletView
-import io.usoamic.webwallet.enums.Transfer
-import io.usoamic.webwallet.other.Timestamp
-import io.usoamic.webwallet.util.TxUtils
 import js.externals.datatables.net.JQueryDataTable
 import js.externals.datatables.net.extension.dataTable
 import js.externals.datatables.net.model.DataTableOption
@@ -20,33 +15,13 @@ class DashboardView(application: Application) : WalletView(application) {
     private val lastTransfersTable = jQuery("#last_transfers").unsafeCast<JQueryDataTable>()
 
     init {
-        prepareTable()
-    }
-
-    private fun prepareTable() {
         prepareLastTransfers()
     }
 
     private fun prepareLastTransfers() {
         lastTransfersTable.dataTable(DataTableOption.initEmpty())
-        methods.getTransactionsByAddress(address, 10) {
-            val list = mutableListOf<List<Any>>()
-            it.forEach { tx ->
-                val txType = TxUtils.getTxType(address, tx.from)
-                list.add(
-                    listOf(
-                        txType.toPlainString(),
-                        when (txType) {
-                            Transfer.DEPOSIT -> tx.from
-                            Transfer.WITHDRAWAL -> tx.to
-                        },
-                        Coin.fromSat(tx.value).toPlainString(),
-                        Timestamp.fromBigNumber(tx.timestamp).toLocaleString()
-                    )
-                )
-            }
-            val options = DataTableOption(list)
-            lastTransfersTable.dataTable(options)
+        getTransactions(10) {
+            lastTransfersTable.dataTable(DataTableOption(it))
         }
     }
 
