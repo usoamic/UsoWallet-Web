b1e882957ccdc326d9f41d542dd988e50f7485a6
primys <a2uso@tuta.io>
2019-09-08 11:57:33 -0700

    Fix issues and add new Transfer types

--- src/main/kotlin/io/usoamic/webwallet/util/TxUtils.kt
+++ src/main/kotlin/io/usoamic/webwallet/util/TxUtils.kt
@@ -4,8 +4,16 @@ import io.usoamic.webwallet.enumcls.Transfer
 
 class TxUtils {
     companion object {
-        fun getTxType(owner: String, from: String): Transfer {
-            return if(owner == from) Transfer.DEPOSIT else Transfer.WITHDRAWAL
+        fun getTxType(owner: String, from: String, to: String): Transfer {
+            val lcOwner = owner.toLowerCase()
+            val lcFrom = from.toLowerCase()
+            val lcTo = to.toLowerCase()
+            return when {
+                ((lcOwner != lcFrom) && (lcOwner == lcTo)) -> Transfer.DEPOSIT
+                ((lcOwner == lcFrom) && (lcOwner != lcTo)) -> Transfer.WITHDRAWAL
+                ((lcOwner == lcFrom) && (lcOwner == lcTo)) -> Transfer.INTERNAL
+                else -> Transfer.UNKNOWN
+            }
         }
     }
 }
