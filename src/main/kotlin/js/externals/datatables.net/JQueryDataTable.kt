92cf7e4e9d186443f87df4337dcadf6653526e7c
primys <a2uso@tuta.io>
2019-10-13 12:36:43 -0700

    js-externals move to separate library

--- src/main/kotlin/js/externals/datatables.net/JQueryDataTable.kt
+++
@@ -1,9 +0,0 @@
-package js.externals.datatables.net
-
-import js.externals.jquery.JQuery
-import org.w3c.dom.HTMLElement
-
-external interface JQueryDataTable : js.externals.jquery.JQuery<HTMLElement> {
-    fun dataTable(json: String)
-    fun dataTable()
-}
--- src/main/kotlin/js/externals/qrcode/QRCodeOptions.kt
+++
@@ -1,7 +0,0 @@
-package js.externals.qrcode
-
-data class QRCodeOptions(
-    val errorCorrectionLevel: String = "M",
-    val maskPattern: Int = 0,
-    val scale: Int = 4
-)
