3d09d33b4ebe6344b7441fb38c662040148c2429
primys <a2uso@tuta.io>
2020-06-16 16:02:08 -0700

    Some refactorings

--- src/main/kotlin/io/usoamic/webwallet/view/WithdrawView.kt
+++ src/main/kotlin/io/usoamic/webwallet/view/WithdrawView.kt
@@ -1,6 +1,7 @@
 package io.usoamic.webwallet.view
 
 import io.usoamic.usoamicktjs.other.Contract
+import io.usoamic.web3kt.bignumber.BigNumber
 import io.usoamic.web3kt.bignumber.BigNumberValue
 import io.usoamic.web3kt.bignumber.extension.toBigNumber
 import io.usoamic.web3kt.buffer.Buffer
@@ -10,6 +11,7 @@ import io.usoamic.web3kt.core.contract.model.EstimateGasOption
 import io.usoamic.web3kt.core.contract.util.Coin
 import io.usoamic.web3kt.tx.Tx
 import io.usoamic.web3kt.tx.block.DefaultBlockParameterName
+import io.usoamic.web3kt.tx.gas.DefaultGasProvider
 import io.usoamic.web3kt.tx.model.RawTransaction
 import io.usoamic.web3kt.util.EthUnit
 import io.usoamic.web3kt.util.extension.addHexPrefixIfNotExist
@@ -20,6 +22,7 @@ import io.usoamic.webwallet.base.Application
 import io.usoamic.webwallet.base.WalletView
 import io.usoamic.webwallet.enumcls.Asset
 import io.usoamic.webwallet.exception.ValidateUtilException
+import io.usoamic.webwallet.model.TxData
 import io.usoamic.webwallet.util.Alert
 import io.usoamic.webwallet.util.ValidateUtil
 import js.externals.jquery.JQuery
@@ -33,12 +36,19 @@ class WithdrawView(application: Application) : WalletView(application) {
 
     private val addressInput = jQuery("#withdraw_address_input")
     private val amountInput = jQuery("#withdraw_amount_input")
+    private val gasPriceSelect = jQuery("#gas_price_input")
     private val passwordInput = jQuery("#withdraw_password_input")
+
     private val withdrawEthBtn = jQuery("#withdraw_eth_button")
     private val withdrawUsoBtn = jQuery("#withdraw_uso_button")
 
     init {
         setListeners()
+
+        web3.eth.getGasPrice()
+            .then { gasPrice ->
+                println("gasPrice: $gasPrice")
+            }.catch(::onException)
     }
 
     private fun setListeners() {
@@ -58,7 +68,7 @@ class WithdrawView(application: Application) : WalletView(application) {
 
     private fun startLoading(asset: Asset) {
         passwordInput.clearVal()
-        when(asset) {
+        when (asset) {
             Asset.COIN -> {
                 withdrawUsoBtn.disable()
                 withdrawEthBtn.startLoading()
@@ -71,7 +81,7 @@ class WithdrawView(application: Application) : WalletView(application) {
     }
 
     private fun stopLoading(asset: Asset) {
-        when(asset) {
+        when (asset) {
             Asset.COIN -> {
                 withdrawUsoBtn.enable()
                 withdrawEthBtn.stopLoading()
@@ -95,6 +105,7 @@ class WithdrawView(application: Application) : WalletView(application) {
         val sAddress = addressInput.content()
         val sAmount = amountInput.content()
         val sPassword = passwordInput.content()
+        val sGasPrice = gasPriceSelect.content()
 
         try {
             ValidateUtil.validateAddress(sAddress)
@@ -102,91 +113,133 @@ class WithdrawView(application: Application) : WalletView(application) {
                 .validatePassword(sPassword)
 
             startLoading(asset)
+            println("Start Loading...")
+            web3.eth.getTransactionCount(address, DefaultBlockParameterName.LATEST)
+                .then { nonce ->
+                    println("Get Transaction Count...")
+                    if (sGasPrice == "auto") {
+                        web3.eth.getGasPrice()
+                            .then { gasPrice ->
+                                println("Get Gas Price...")
+                                sendWith(
+                                    TxData(
+                                        nonce = nonce,
+                                        gasPrice = gasPrice,
+                                        asset = asset,
+                                        address = sAddress,
+                                        amount = sAmount,
+                                        password = sPassword
+                                    )
+                                )
+                            }
+                            .catch(::onException)
+                    } else {
+                        val gasPrice = when (sGasPrice) {
+                            "125" -> DefaultGasProvider.GAS_PRICE_125
+                            "100" -> DefaultGasProvider.GAS_PRICE_100
+                            "75" -> DefaultGasProvider.GAS_PRICE_75
+                            "50" -> DefaultGasProvider.GAS_PRICE_50
+                            "35" -> DefaultGasProvider.GAS_PRICE_35
+                            "20" -> DefaultGasProvider.GAS_PRICE_20
+                            else -> throw Exception("Invalid Gas Price")
+                        }
+                        sendWith(
+                            TxData(
+                                nonce = nonce,
+                                gasPrice = gasPrice,
+                                asset = asset,
+                                address = sAddress,
+                                amount = sAmount,
+                                password = sPassword
+                            )
+                        )
+                    }
+                }
+                .catch(::onException)
 
-            when (asset) {
-                Asset.COIN -> sendEth(sAddress, sAmount, sPassword)
-                Asset.TOKEN -> sendUso(sAddress, sAmount, sPassword)
-            }
         } catch (t: Throwable) {
             onException(t)
         }
     }
 
-    private fun sendEth(sAddress: String, sAmount: String, sPassword: String) {
-        web3.eth.getTransactionCount(address, DefaultBlockParameterName.LATEST)
-            .then { nonce ->
-                val value = web3.utils.toWei(sAmount, EthUnit.ETHER).toBigNumber()
-                web3.eth.estimateGas(
-                    EstimateGasOption(
-                        address,
-                        sAddress,
-                        value
-                    ))
-                    .then { gasLimit ->
-                        val transaction = RawTransaction.createEtherTransaction(
-                            address,
-                            nonce,
-                            gasLimit,
-                            sAddress,
-                            value
-                        )
-                        sendTransaction(Asset.COIN, transaction, sPassword)
-                    }
-                    .catch {
-                        onException(it)
-                    }
-            }
-            .catch {
-                onException(it)
-            }
+    private fun sendWith(
+        data: TxData
+    ) {
+        println("SendWithGasPrice: ${data.gasPrice}")
+        when (data.asset) {
+            Asset.COIN -> sendEth(data)
+            Asset.TOKEN -> sendUso(data)
+        }
     }
 
-    private fun sendUso(sAddress: String, sAmount: String, sPassword: String) {
-        web3.eth.getTransactionCount(address, DefaultBlockParameterName.LATEST)
-            .then { nonce ->
-                val response = methods.transfer(sAddress, Coin.fromCoin(sAmount).toStringSat())
-                response.estimateGas(
-                    EstimateGasOption(
-                        address,
-                        sAddress,
-                        BigNumberValue.ZERO,
-                        response.encodeABI()
-                    )
+    private fun sendEth(
+        data: TxData
+    ) {
+        val value = web3.utils.toWei(data.amount, EthUnit.ETHER).toBigNumber()
+        web3.eth.estimateGas(
+            EstimateGasOption(
+                address,
+                data.address,
+                value
+            )
+        )
+            .then { gasLimit ->
+                println("Get Gas Limit...")
+                val transaction = RawTransaction.createEtherTransaction(
+                    from = address,
+                    nonce = data.nonce,
+                    gasPrice = data.gasPrice,
+                    gasLimit = gasLimit,
+                    to = data.address,
+                    value = value
                 )
-                    .then { gasLimit ->
-                        val transaction = RawTransaction.createContractTransaction(
-                            address,
-                            nonce,
-                            gasLimit,
-                            Contract.forNetwork(AppConfig.NETWORK),
-                            response.encodeABI()
-                        )
-                        sendTransaction(Asset.TOKEN, transaction, sPassword)
-                    }
-                    .catch {
-                        onException(it)
-                    }
+                sendTransaction(Asset.COIN, transaction, data.password)
             }
-            .catch {
-                onException(it)
+            .catch(::onException)
+    }
+
+    private fun sendUso(
+        data: TxData
+    ) {
+        val response = methods.transfer(data.address, Coin.fromCoin(data.amount).toStringSat())
+        response.estimateGas(
+            EstimateGasOption(
+                address,
+                data.address,
+                BigNumberValue.ZERO,
+                response.encodeABI()
+            )
+        )
+            .then { gasLimit ->
+                println("Get Gas Limit...")
+                val transaction = RawTransaction.createContractTransaction(
+                    from = address,
+                    nonce = data.nonce,
+                    gasPrice = data.gasPrice,
+                    gasLimit = gasLimit,
+                    to = Contract.forNetwork(AppConfig.NETWORK),
+                    data = response.encodeABI()
+                )
+                sendTransaction(Asset.TOKEN, transaction, data.password)
             }
+            .catch(::onException)
     }
 
     private fun sendTransaction(asset: Asset, transaction: RawTransaction, password: String) {
+        println("Signing...")
         val privateKey = Wallet.fromV3(account, password).getPrivateKeyAsString().removeHexPrefixIfExist()
-
         val tx = Tx(transaction)
         tx.sign(Buffer.fromHex(privateKey))
 
         val signedTransaction = tx.serialize()
+        println("Sending...")
         web3.eth.sendSignedTransaction(signedTransaction.toHex().addHexPrefixIfNotExist())
             .then {
+                println("Sent!")
                 Alert.show(it.transactionHash)
                 stopLoading(asset)
             }
-            .catch {
-                onException(it)
-            }
+            .catch(::onException)
     }
 
     override fun onStop() {
