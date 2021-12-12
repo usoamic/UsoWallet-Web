e9323c6421d8f23bf6e9c7a90e132212166a76ee
primys <primys@usoamic.io>
2021-12-12 04:28:09 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,10 @@ object Version {
 }
 
 buildscript {
-    val kotlinVersion = "1.3.72"
+    val kotlinVersion = "1.4.32"
 
     repositories {
-        jcenter()
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+        mavenCentral()
     }
 
     dependencies {
@@ -26,18 +25,17 @@ buildscript {
 }
 
 plugins {
-    val kotlinVersion = "1.3.72"
-    id("org.jetbrains.kotlin.js") version kotlinVersion
+    kotlin("js") version "1.4.32"
 }
 
 allprojects {
     group = "io.usoamic"
-    version = "1.1.2"
+    version = "1.2.0"
 }
 
 repositories {
     mavenCentral()
-    maven ("https://kotlin.bintray.com/js-externals")
+
     flatDir {
         dirs("libs")
     }
@@ -46,23 +44,23 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-js"))
 
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core-js", "1.3.0")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core-js", "1.4.3")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation(npm("web3", "^1.2.9"))
-    implementation(npm("bignumber.js", "^9.0.0"))
-    implementation(npm("datatables.net-bs4", "^1.10.21"))
-    implementation(npm("ethereumjs-wallet", "^0.6.3"))
-    implementation(npm("ethereumjs-tx", "^1.3.7"))
+    implementation(npm("web3", "^1.6.1"))
+    implementation(npm("bignumber.js", "^9.0.1"))
+    implementation(npm("datatables.net-bs4", "^1.11.3"))
+    implementation(npm("ethereumjs-wallet", "^0.6.5"))
+    implementation(npm("ethereumjs-tx", "^2.1.2"))
     implementation(npm("babel-polyfill", "^6.26.0"))
-    implementation(npm("jquery", "^3.3.1"))
+    implementation(npm("jquery", "^3.6.0"))
     implementation(npm("toastr", "^2.1.4"))
     implementation(npm("qrcode", "^1.4.1"))
-    implementation(npm("bootstrap", "^4.5.0"))
+    implementation(npm("bootstrap", "^4.6.1"))
     implementation(npm("popper.js", "^1.16.0"))
 
-    testImplementation("org.jetbrains.kotlin", "kotlin-test-js")
+    testImplementation(kotlin("kotlin-test-js"))
 }
 
-kotlin.target.browser {}
+kotlin.js().browser {}
 
