8757e4b72cd2e4d25beaedc391062eeebc97eda8
primys <primys@usoamic.io>
2021-12-12 17:45:39 +0300

    Now using versions from object

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 object Version {
-    const val web3 = "^1.2.9"
-    const val bignumberJs = "^9.0.0"
-    const val datatablesNetBs4 = "^1.10.21"
-    const val ethereumJsWallet = "^0.6.3"
+    const val web3 = "^1.6.1"
+    const val bigNumberJs = "^9.0.1"
+    const val datatablesNetBs4 = "^1.11.3"
+    const val ethereumJsWallet = "^0.6.5"
     const val ethereumJsTx = "^1.3.7"
     const val babelPolyFill = "^6.26.0"
-    const val jQuery = "^3.3.1"
+    const val jQuery = "^3.6.0"
     const val toastr = "^2.1.4"
     const val qrCode = "^1.4.1"
-    const val bootstrap = "^4.5.0"
+    const val bootstrap = "^4.6.1"
     const val popperJs = "^1.16.0"
 }
 
@@ -47,17 +47,17 @@ dependencies {
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core-js", "1.4.3")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation(npm("web3", "^1.6.1"))
-    implementation(npm("bignumber.js", "^9.0.1"))
-    implementation(npm("datatables.net-bs4", "^1.11.3"))
-    implementation(npm("ethereumjs-wallet", "^0.6.5"))
-    implementation(npm("ethereumjs-tx", "^2.1.2"))
-    implementation(npm("babel-polyfill", "^6.26.0"))
-    implementation(npm("jquery", "^3.6.0"))
-    implementation(npm("toastr", "^2.1.4"))
-    implementation(npm("qrcode", "^1.4.1"))
-    implementation(npm("bootstrap", "^4.6.1"))
-    implementation(npm("popper.js", "^1.16.0"))
+    implementation(npm("web3", Version.web3))
+    implementation(npm("bignumber.js", Version.bigNumberJs))
+    implementation(npm("datatables.net-bs4", Version.datatablesNetBs4))
+    implementation(npm("ethereumjs-wallet", Version.ethereumJsWallet))
+    implementation(npm("ethereumjs-tx", Version.ethereumJsTx))
+    implementation(npm("babel-polyfill", Version.babelPolyFill))
+    implementation(npm("jquery", Version.jQuery))
+    implementation(npm("toastr", Version.toastr))
+    implementation(npm("qrcode", Version.qrCode))
+    implementation(npm("bootstrap", Version.bootstrap))
+    implementation(npm("popper.js", Version.popperJs))
 
     testImplementation(kotlin("test-js"))
 }
